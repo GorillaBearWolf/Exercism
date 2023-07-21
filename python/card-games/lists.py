f0d5015ec4e0de5bd974e582ba21d6caf72a0709
Matthew Refsnider <mrefsnider@icloud.com>
2023-07-20 20:43:28 -0400

    completed card-games

--- python/card-games/lists.py
+++ python/card-games/lists.py
@@ -11,7 +11,7 @@ def get_rounds(number):
     :return: list - current round and the two that follow.
     """
 
-    pass
+    return [number, number + 1, number + 2]
 
 
 def concatenate_rounds(rounds_1, rounds_2):
@@ -22,7 +22,7 @@ def concatenate_rounds(rounds_1, rounds_2):
     :return: list - all rounds played.
     """
 
-    pass
+    return rounds_1 + rounds_2
 
 
 def list_contains_round(rounds, number):
@@ -33,7 +33,7 @@ def list_contains_round(rounds, number):
     :return: bool - was the round played?
     """
 
-    pass
+    return number in rounds
 
 
 def card_average(hand):
@@ -43,7 +43,7 @@ def card_average(hand):
     :return: float - average value of the cards in the hand.
     """
 
-    pass
+    return sum(hand) / len(hand)
 
 
 def approx_average_is_average(hand):
@@ -53,7 +53,14 @@ def approx_average_is_average(hand):
     :return: bool - does one of the approximate averages equal the `true average`?
     """
 
-    pass
+    first_last_average = (hand[0] + hand[-1]) / 2
+    median_of_hand = hand[len(hand) // 2]
+
+    if first_last_average == card_average(hand):
+        return True
+    if median_of_hand == card_average(hand):
+        return True
+    return False
 
 
 def average_even_is_average_odd(hand):
@@ -63,7 +70,10 @@ def average_even_is_average_odd(hand):
     :return: bool - are even and odd averages equal?
     """
 
-    pass
+    even_average = card_average(hand[::2])
+    odd_average = card_average(hand[1::2])
+
+    return odd_average == even_average
 
 
 def maybe_double_last(hand):
@@ -73,4 +83,7 @@ def maybe_double_last(hand):
     :return: list - hand with Jacks (if present) value doubled.
     """
 
-    pass
+    if hand[-1] == 11:
+        hand[-1] = 22
+        return hand
+    return hand
