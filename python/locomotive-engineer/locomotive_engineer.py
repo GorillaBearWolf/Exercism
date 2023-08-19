ab8307adc5a90393ec45cffb9ba2889d16d2062c
Matthew Refsnider <mrefsnider@icloud.com>
2023-08-18 20:08:43 -0400

    wip fix_wagon_depot

--- python/locomotive-engineer/locomotive_engineer.py
+++ python/locomotive-engineer/locomotive_engineer.py
@@ -51,4 +51,5 @@ def fix_wagon_depot(wagons_rows):
     :param wagons_rows: list[list[tuple]] - the list of rows of wagons.
     :return: list[list[tuple]] - list of rows of wagons.
     """
-    pass
+    a, b, c = zip(*wagons_rows)
+    
