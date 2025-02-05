f18fe3b18b031d28788778ca038364d797682cf5
mrefsnider <mrefsnider@orangetheory.com>
2025-02-05 01:22:11 -0500

    add protein_translation

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 def proteins(strand):
     protein_dict = {
         'AUG': 'Methionine',
@@ -33,3 +34,27 @@ def proteins(strand):
 
 
     return protein_name_list
+||||||| Stash base
+=======
+def translate_proteins(rna):
+    proteins = []
+    for i in range(0, len(rna), 3):
+        if ("UAA", "UAG", "UGA") in rna[i:i + 3]:
+            return proteins
+        if ("AUG") in rna[i:i + 3]:
+            proteins.append("Methionine")
+        if ("UUU", "UUC") in rna[i:i + 3]:
+            proteins.append("Phenylalanine")
+        if ("UUA", "UUG") in rna[i:i + 3]:
+            proteins.append("Leucine")
+        if ("UCU", "UCC", "UCA", "UCG") in rna[i:i + 3]:
+            proteins.append("Serine")
+        if ("UAU", "UAC") in rna[i:i + 3]:
+            proteins.append("Tyrosine")
+        if ("UGU", "UGC") in rna[i:i + 3]:
+            proteins.append("Cysteine")
+        if ("UGG") in rna[i:i + 3]:
+            proteins.append("Tryptophan")
+
+    return proteins
+>>>>>>> Stashed changes
